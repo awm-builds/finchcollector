5562513eed30f2cc6feff1933ded1d76de8686fd
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-06-03 09:50:36 -0400

    -Added update and delete functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 finches = [
@@ -31,3 +31,12 @@ class FinchCreate(CreateView):
   model = Finch
   fields = '__all__'
   success_url = '/finches/{id}'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
